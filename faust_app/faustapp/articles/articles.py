1a100edc699a181da7a7ffd08cb2f88941088c59
Eric Bogard <ericbogard@Erics-MacBook-Pro.local>
2020-10-19 17:04:04 -0400

    Articles fetcher

--- faust_app/faustapp/articles/articles.py
+++ faust_app/faustapp/articles/articles.py
@@ -1,6 +1,32 @@
+import shutil
+import os
 import faust
+from faust.web import Request, Response, View
 from faustapp.app import app
+from git import Repo
 
 topic = app.topic('articles')
+
+def handle_error(func, path, execinfo):
+    print('Unable remove directory' + path)
+
+@app.page('/articles/')
+class process(View):
+    async def post(self, request: Request) -> Response:
+        repo_name = 'temp_repo'
+        print("Cloning articles repository...")
+        Repo.clone_from("https://github.com/vandenbogart/art.vandenbog.articles.git", repo_name)
+        with os.scandir(repo_name) as entries:
+            for entry in entries:
+                if entry.is_file():
+                    with open(entry, 'r') as f:
+                        data = f.read()
+                        print(entry.name)
+                        print(data)
+        print("Cleaning up temporary files...")
+        shutil.rmtree(repo_name, onerror=handle_error) 
+        return self.json({'error': None})
+
+
 
 
--- faust_app/setup.py
+++ faust_app/setup.py
@@ -4,6 +4,7 @@ requires = [
     "faust",
     "yarl<1.6.0,>=1.0",
     "multidict<5.0,>=4.5",
+    "gitpython"
 ]
 
 setup(
