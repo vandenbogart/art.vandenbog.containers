3dae597284a0f0991e13f34ba426ad38bbf0ecc6
Eric Bogard <ericbogard@Erics-MacBook-Pro.local>
2020-10-22 12:21:12 -0400

    Reworking article fetcher

--- faust_app/faustapp/articles/articles.py
+++ faust_app/faustapp/articles/articles.py
@@ -1,19 +1,21 @@
 import shutil
-import requests
 import os
 import faust
 from bs4 import BeautifulSoup
 from faust.web import Request, Response, View
 from faustapp.app import app
 from git import Repo
+from datetime import datetime
 
-gitrepo = 'https://github.com/vandenbogart/art.vandenbog.articles.git'
+articles_repo = "https://github.com/vandenbogart/art.vandenbog.articles.git"
 
 class Article(faust.Record):
     title: str
     filename: str
     html: str
     deleted: bool = False
+    created: datetime
+    last_modified: datetime
 
 topic = app.topic('articles', key_type=str, value_type=Article)
 
@@ -27,35 +29,26 @@ articles_table = app.Table(
 def handle_error(func, path, execinfo):
     print('Unable remove directory' + path)
 
-@app.page('/reconcile/')
-class reconcile_articles(View):
+@app.page('/publish/')
+class publish_articles(View):
     async def post(self, request: Request) -> Response:
-
-        changelog = await request.text()
-        print(changelog)
-        split_lines = changelog.split('\n')
-        # drop metadata about sha
-        split_lines.pop(0)
-        for item in split_lines:
-            op, filename = item.split('\t')
-            if op == 'A' or op == 'M' or op == 'D':
-                print('Detected changed file: ' + op + ' ' + filename)
-                print('Fetching updated version...')
-                article = requests.get(gitrepo + filename)
-                print('Parsing file...')
-                soup = BeautifulSoup(article.text, 'html.parser')
-                print('Publishing change to topic...')
-                await topic.send(key=filename,
-                            value=Article(
-                                title=soup.title.string,
-                                filename=filename,
-                                html=article.text,
-                                deleted=True if op == 'D' else False
-                                )
-                            )
-            else:
-                print('Unrecognized operation: ' + op + ' ignoring line.')
-                continue
+        repo_name = 'temp_repo'
+        print("Cloning articles repository...")
+        shutil.rmtree(repo_name, ignore_errors=True) 
+        repo = Repo.clone_from(articles_repo, repo_name)
+        with os.scandir(repo_name) as entries:
+            for entry in entries:
+                if entry.is_file():
+                    last_modified = repo.git.log('-1', format='%ci', 'a_good_time.html')
+                    print(last_modified)
+                    with open(entry, 'r') as f:
+                        data = f.read()
+                        soup = BeautifulSoup(data, 'html.parser')
+                        print(soup.title.string)
+                        print(data)
+                        #await topic.send(key=entry.name, value=Article(title=soup.title.string, filename=entry.name, html=data))
+        print("Cleaning up temporary files...")
+        shutil.rmtree(repo_name, onerror=handle_error) 
         return self.json({'error': None})
 
 @app.page('/articles/{word}')
